b232bbe54d6a8970b35bccd4b9a4fb313aa6044d
Chris Scholz <chris.scholz@electric.ai>
2022-09-15 10:21:42 -0400

    Basic king movement

--- src/com/chess/engine/pieces/King.java
+++ src/com/chess/engine/pieces/King.java
@@ -2,14 +2,22 @@ package com.chess.engine.pieces;
 
 import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
+import com.chess.engine.board.BoardUtils;
 import com.chess.engine.board.Move;
+import com.chess.engine.board.Tile;
+import com.google.common.collect.ImmutableList;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
-public abstract class King extends Piece {
+public class King extends Piece {
     private final static int[] CANDIDATE_MOVE_COORDINATES = { -9, -8, -7, -1, 1, 7, 8, 9 };
+    private boolean isInCheck;
 
-    public abstract boolean isInCheck();
+    public boolean isInCheck() {
+        return this.isInCheck;
+    }
 
     King(final int piecePosition, final Alliance pieceAlliance) {
         super(piecePosition, pieceAlliance);
@@ -17,6 +25,46 @@ public abstract class King extends Piece {
 
     @Override
     public Collection<Move> calculateLegalMoves(Board board) {
-        return null;
+        final List<Move> legalMoves = new ArrayList<>();
+
+        for(final int currentCandidateOffset : CANDIDATE_MOVE_COORDINATES) {
+            final int candidateDestinationCoordinate = this.piecePosition + currentCandidateOffset;
+            if(BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
+                // TODO special instance: king must move out of check and cannot move into check
+                // TODO castling
+
+                if(isFirstColumnExclusion(this.piecePosition, currentCandidateOffset) ||
+                   isEighthColumnExclusion(this.piecePosition, currentCandidateOffset)) {
+                    continue;
+                }
+
+                final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
+
+                if(!candidateDestinationTile.isTileOccupied()) {
+                    legalMoves.add(new Move.MajorMove(board, this, candidateDestinationCoordinate));
+                } else {
+                    final Piece pieceAtDestination = candidateDestinationTile.getPiece();
+                    final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance();
+                    if(this.pieceAlliance != pieceAlliance) { // Piece is an enemy piece
+                        legalMoves.add(new Move.AttackMove(board,
+                                this,
+                                candidateDestinationCoordinate,
+                                pieceAtDestination));
+                    }
+                }
+            }
+        }
+
+        return ImmutableList.copyOf(legalMoves);
+    }
+
+    private static boolean isFirstColumnExclusion(final int currentPosition, final int candidateOffset){
+        return BoardUtils.INSTANCE.FIRST_COLUMN.get(currentPosition) &&
+                (candidateOffset == -1 || candidateOffset == -9 || candidateOffset == 7);
+    }
+
+    private static boolean isEighthColumnExclusion(final int currentPosition, final int candidateOffset) {
+        return BoardUtils.INSTANCE.EIGHTH_COLUMN.get(currentPosition) &&
+                (candidateOffset == 1 || candidateOffset == -7 || candidateOffset == 9);
     }
 }
