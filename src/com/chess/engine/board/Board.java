0db2baf003da66c2dd117135fbaa5fc0f0ecba1f
Chris Scholz <chris.scholz@electric.ai>
2022-09-15 15:51:10 -0400

    Stub player classes, add MoveStatus and MoveTransition

--- src/com/chess/engine/board/Board.java
+++ src/com/chess/engine/board/Board.java
@@ -3,6 +3,7 @@ package com.chess.engine.board;
 import com.chess.engine.Alliance;
 import com.chess.engine.pieces.*;
 import com.chess.engine.player.BlackPlayer;
+import com.chess.engine.player.Player;
 import com.chess.engine.player.WhitePlayer;
 import com.google.common.collect.ImmutableList;
 
@@ -66,6 +67,14 @@ public class Board {
             }
         }
         return ImmutableList.copyOf(activePieces);
+    }
+
+    public Player getBlackPlayer() {
+        return this.blackPlayer;
+    }
+
+    public Player getWhitePlayer() {
+        return this.whitePlayer;
     }
 
     public Tile getTile(final int tileCoordinate) {
--- src/com/chess/engine/player/Player.java
+++ src/com/chess/engine/player/Player.java
@@ -1,5 +1,6 @@
 package com.chess.engine.player;
 
+import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
 import com.chess.engine.board.Move;
 import com.chess.engine.pieces.King;
@@ -30,5 +31,36 @@ public abstract class Player {
         throw new RuntimeException("Board not in valid state. No king found.");
     }
 
+    public boolean isMoveLegal(final Move move) {
+        return this.legalMoves.contains(move);
+    }
+
+    // TODO: implement
+    public boolean isInCheck() {
+        return false;
+    }
+
+    // TODO: implement
+    public boolean isInCheckmate() {
+        return false;
+    }
+
+    // TODO: implement
+    public boolean isInStalemate() {
+        return false;
+    }
+
+    // TODO: implement
+    public boolean isCastled() {
+        return false;
+    }
+
+    // TODO: implement
+    public MoveTransition makeMove(final Move move) {
+        return null;
+    }
+
     public abstract Collection<Piece> getActivePieces();
+    public abstract Alliance getAlliance();
+    public abstract Player getOpponent();
 }
